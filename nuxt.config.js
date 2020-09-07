993ab61b92eb88a381318dd4ba418d26e4d3b20b
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-09-07 01:23:27 -0400

    install nuxt and related libs, update nuxt and tailwind config

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+const path = require('path').join
+const tailwindcss = path(__dirname, 'tailwind.config.js')
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    // '~assets/css/tailwind.css',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    "@nuxt/content",
+    'nuxt-purgecss'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      plugins: [
+        require('tailwindcss')(tailwindcss), 
+        require('autoprefixer')
+      ]
+    },
+    preset: {
+      autoprefixer: {
+        grid: true
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  },
+
+  purgeCSS: {
+    mode: 'postcss',
+    enabled: (process.env.NODE_ENV === 'production') //TODO: add env later
+  },
+  /*
+  ** Server configuration
+  */
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px'
+    },
+    colors: {
+      indigo: '#5c6ac4',
+      blue: '#007ace',
+      red: {
+        regular: '#de3618',
+        dark: '#A52A2A'
+      },
+      pink: {
+        light: '#FFB6C1'
+      }
+    },
+    fontFamily: {
+      default: ['OpenSans', 'sans-serif']
+    }
+  }
+}
