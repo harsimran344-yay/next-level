985851aad2aba587faa467e37656e34787227ccb
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-09-21 13:53:42 -0400

    add header and nav bar basic styles

--- components/Header.vue
+++ components/Header.vue
@@ -4,14 +4,22 @@
     export default {
         components: {
             NavBar
+        },
+        data() {
+            return {
+                isOpen: false,
+            }
         }
     }
 </script>>
 
 <template>
     <!-- TODO: setup a common content for header as well -->
-    <div class="px-10 pb-2 border-b-2 border-red-dark flex justify-between">
-        <img class="w-40 h-20" src="~/assets/images/next-level-logo.png" />
-        <NavBar />
-    </div>
+    <header class="px-10 pb-2 border-b-2 border-red-dark tablet:flex tablet:justify-between tablet:items-end">
+        <div class="flex items-end justify-between">
+            <img class="w-40 h-20" src="~/assets/images/next-level-logo.png" />
+            <button @click="isOpen = !isOpen" type="button" class="tablet:hidden">Menu</button>
+        </div>
+        <NavBar :class="isOpen ? 'block' : 'hidden'" class="tablet:flex"/>
+    </header>
 </template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,9 +3,9 @@
         <nav>
             <!-- TODO: create common content for nav lists - home, about and contant. 
             plane text will be deleted -->
-            <nuxt-link to="/" class="flex px-2 py-1">HOME</nuxt-link>
-            <nuxt-link to="/about" class="flex px-2 py-1">ABOUT</nuxt-link>
-            <nuxt-link to="/contact" class="flex px-2 py-1">CONTACT</nuxt-link>
+            <nuxt-link to="/" class="block px-2 py-1">HOME</nuxt-link>
+            <nuxt-link to="/about" class="block px-2 py-1">ABOUT</nuxt-link>
+            <nuxt-link to="/contact" class="block px-2 py-1">CONTACT</nuxt-link>
         </nav>
     <!-- </div> -->
 </template>
